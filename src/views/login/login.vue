aec624652bc2c71d55e84a29004ad4e3c1953fb6
hevin <hevin@dingding.com>
2020-05-26 18:00:35 +0800

    代码注释

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -37,21 +37,15 @@
           </div>
         </div>
         <!-- <div class="toggle--left"
-          @click="!loading ? 
-              (loginForm.loginName = '',loginForm.password = '',
-                  tollLogin = !tollLogin) : null">
-          收费员登录
-        </div> -->
-        <div class="toggle--left"
           @click="loginToll">
           收费员登录
         </div>
         <div class="toggle"
           @click="!loading ? merCodeLogin = !merCodeLogin : null">
           {{ merCodeLogin ? '转至运营商登录' : '转至商户登录' }}
-        </div>
+        </div> -->
       </template>
-      <template v-else>
+      <!-- <template v-else>
         <div class="bottom bottom--large">
           <input-group ref="inputGroup">
             <input-mercode :input-box="merCodeInputBox"
@@ -117,72 +111,68 @@
             </div>
           </div>
         </div>
-        <!-- <div class="toggle--left"
-          @click="!loading ? tollLogin = !tollLogin : null">
-          返回
-        </div> -->
         <div class="toggle--left"
           @click="back">
           返回
         </div>
-      </template>
+      </template> -->
     </div>
   </div>
 </template>
 <script>
-import { InputGroup, InputBox, InputSelect, InputMercode } from '@/components'
+import { InputGroup, InputBox, InputSelect, InputMercode } from "@/components";
 export default {
   data() {
     return {
       loading: false,
       loginForm: {
-        loginName: '',
-        password: '',
-        merCode: ''
+        loginName: "",
+        password: "",
+        merCode: ""
       },
       tollLogin: false, //收费员登录面板
       merCodeLogin: true, // 默认商户登录
       loginActive: false,
       userInfo: {
-        merCode: '',
-        merName: '',
-        token: '',
+        merCode: "",
+        merName: "",
+        token: "",
         parkInfo: {
-          parkCode: '',
-          parkName: '',
-          gwType: '',
-          gwCode: ''
+          parkCode: "",
+          parkName: "",
+          gwType: "",
+          gwCode: ""
         }
       },
       userNameInputBox: {
-        placeholder: '请输入用户名',
-        errorText: '用户名不能为空',
-        iconfont: 'icon-yonghu',
+        placeholder: "请输入用户名",
+        errorText: "用户名不能为空",
+        iconfont: "icon-yonghu",
         pattern: /^[a-zA-z][a-zA-Z0-9_-]{3,15}$/,
-        errorText2: '请正确填写用户名'
+        errorText2: "请正确填写用户名"
       },
       passwordInputBox: {
-        placeholder: '请输入密码',
-        errorText: '密码不能为空',
-        type: 'password',
-        iconfont: 'icon-icon-mima',
+        placeholder: "请输入密码",
+        errorText: "密码不能为空",
+        type: "password",
+        iconfont: "icon-icon-mima",
         pattern: /^\w{4,15}$/,
-        errorText2: '请正确输入密码'
+        errorText2: "请正确输入密码"
       },
       merCodeInputBox: {
-        placeholder: '请输入商户名',
-        iconfont: 'icon-icon-home',
-        errorText: '请正确选择商户名'
+        placeholder: "请输入商户名",
+        iconfont: "icon-icon-home",
+        errorText: "请正确选择商户名"
       }
-    }
+    };
   },
   created() {
-    if (localStorage.getItem('phton_merCodeLogin')) {
+    if (localStorage.getItem("phton_merCodeLogin")) {
       this.merCodeLogin = JSON.parse(
-        localStorage.getItem('phton_merCodeLogin')
-      )
+        localStorage.getItem("phton_merCodeLogin")
+      );
     }
-    this.tollLogin = this.$route.params.type === 'toll'
+    this.tollLogin = this.$route.params.type === "toll";
   },
   methods: {
     /**
@@ -191,7 +181,7 @@ export default {
     loginToll() {
       if (!this.loading) {
         //解决闸机 websocket ip+端口的模式在 wss 协议下不可用问题
-        location.href = `http://${location.host}/#/login/toll`
+        location.href = `http://${location.host}/#/login/toll`;
       }
     },
     /**
@@ -199,79 +189,79 @@ export default {
      */
     back() {
       if (!this.loading) {
-        history.back()
+        history.back();
       }
     },
     /**
      * 商户选择回调
      */
     onMerSelect(merchant) {
-      this.userInfo.merName = merchant.merName || {}
+      this.userInfo.merName = merchant.merName || {};
     },
     /**
      * 登录
      */
     login() {
       if (this.$refs.inputGroup.validate()) {
-        this.loading = true
-        let result
+        this.loading = true;
+        let result;
         if (this.tollLogin) {
-          result = this.axios.post('v1/login/sentryBoxTollLogin', {
+          result = this.axios.post("v1/login/sentryBoxTollLogin", {
             ...this.loginForm,
             parkCode: this.userInfo.parkInfo.parkCode
-          }) // 收费员登录
+          }); // 收费员登录
         } else if (this.merCodeLogin) {
-          result = this.axios.post('v1/login/merchantLogin', this.loginForm) // 商户登录
+          result = this.axios.post("v1/login/merchantLogin", this.loginForm); // 商户登录
         } else {
-          result = this.axios.post('v1/login/operatorLogin', {
+          result = this.axios.post("v1/login/operatorLogin", {
             // 运营商登录
             loginName: this.loginForm.loginName,
             password: this.loginForm.password
-          })
+          });
         }
-        !result && (this.loading = false)
+        !result && (this.loading = false);
         result &&
           result
             .then(response => {
-              this.loading = false
-              localStorage.setItem('phton_merCodeLogin', this.merCodeLogin) // 登陆成功缓存登录模式(是运营商还是商户)
-              response.loginName = this.loginForm.loginName
-              let roles = (response.roleIds || '').split(',')
+              this.loading = false;
+              localStorage.setItem("phton_merCodeLogin", this.merCodeLogin); // 登陆成功缓存登录模式(是运营商还是商户)
+              response.loginName = this.loginForm.loginName;
+              let roles = (response.roleIds || "").split(",");
               if (
-                roles.every(role => role.startWith('2')) &&
+                roles.every(role => role.startWith("2")) &&
                 !this.merCodeLogin
               ) {
                 //运营商登录
-                response.optorCode = response.merCode
-                response.merCode = ''
+                response.optorCode = response.merCode;
+                response.merCode = "";
               }
               if (roles.includes(this.RoleType.ParkMerSentryBoxToll)) {
-                this.userInfo = Object.assign(this.userInfo, response)
-                this.redirect(this.userInfo, roles)
+                this.userInfo = Object.assign(this.userInfo, response);
+                this.redirect(this.userInfo, roles);
               } else {
-                response.merName = this.$utils.isEmpty(response.merName) ?
-                  this.userInfo.merName :
-                  response.merName
-                this.redirect(response, roles)
+                response.merName = this.$utils.isEmpty(response.merName)
+                  ? this.userInfo.merName
+                  : response.merName;
+                this.redirect(response, roles);
               }
             })
-            .catch(error => (this.loading = false))
+            .catch(error => (this.loading = false));
       }
     },
     /**
      * 停车场选择回调
      */
     onParkSelect(park) {
-      this.userInfo.parkInfo.parkCode = park.parkCode
-      this.userInfo.parkInfo.parkName = park.parkName
+      this.userInfo.parkInfo.parkCode = park.parkCode;
+      this.userInfo.parkInfo.parkName = park.parkName;
     },
     /**
      * 重定向
      */
     redirect(userInfo, roles = []) {
-      this.$store.dispatch('updateUser', userInfo)
-      this.$store.dispatch('initSocket')
-      let path = '/'
+      this.$store.dispatch("updateUser", userInfo);
+      this.$store.dispatch("initSocket");
+      let path = "/";
       if (Array.isArray(roles)) {
         if (
           roles.some(
@@ -279,40 +269,40 @@ export default {
               String(role) === this.RoleType.ParkMerAdmin ||
               String(role) === this.RoleType.ParkMerCfg
           ) &&
-          this.$utils.isEmpty(this.$utils.get(userInfo, 'parkInfo.parkCode'))
+          this.$utils.isEmpty(this.$utils.get(userInfo, "parkInfo.parkCode"))
         ) {
-          path = 'property/parking'
+          path = "property/parking";
         } else if (roles.includes(this.RoleType.ParkMerSentryBoxToll)) {
-          path = '/merchant/console'
+          path = "/merchant/console";
         }
       }
       this.$router.replace({
         path: this.$route.query.redirect ? this.$route.query.redirect : path,
-        query: { from: 'login' }
-      })
+        query: { from: "login" }
+      });
     }
   },
   watch: {
     $route(to, from) {
-      this.tollLogin = this.$route.params.type === 'toll'
+      this.tollLogin = this.$route.params.type === "toll";
     },
     loginForm: {
       handler(newVal) {
         this.$nextTick(() => {
-          this.loginActive = this.$refs.inputGroup ?
-            this.$refs.inputGroup.validate(true) :
-            false
-        })
+          this.loginActive = this.$refs.inputGroup
+            ? this.$refs.inputGroup.validate(true)
+            : false;
+        });
       },
       deep: true
     },
-    'userInfo.parkInfo': {
+    "userInfo.parkInfo": {
       handler(newVal) {
         this.$nextTick(() => {
-          this.loginActive = this.$refs.inputGroup ?
-            this.$refs.inputGroup.validate(true) :
-            false
-        })
+          this.loginActive = this.$refs.inputGroup
+            ? this.$refs.inputGroup.validate(true)
+            : false;
+        });
       },
       deep: true
     }
@@ -323,14 +313,14 @@ export default {
     InputMercode,
     InputSelect
   }
-}
+};
 </script>
 
 <style lang="scss">
 .login-page {
   height: 100%;
   color: #5cdbff;
-  background: url('~@/assets/loginBackground.png');
+  background: url("~@/assets/loginBackground.png");
   padding-top: 28vh;
   .login-page-container {
     width: 350px;
@@ -353,7 +343,7 @@ export default {
       // border: 1px solid #5cdbff;
       border-radius: 10px;
       padding: 28px 38px 38px 38px;
-      background: url('~@/assets/loginBottom.png');
+      background: url("~@/assets/loginBottom.png");
       background-repeat: no-repeat;
       background-size: 96% 100%;
       &.bottom--large {
